cc3a05f7a096e2e8e2687976f0aa15b8281228b5
Ryan Gamel <ryan.gamel@gmail.com>
2022-12-02 00:25:26 -0600

    clean up day 1

--- Day-01/1A.js
+++ Day-01/1A.js
@@ -19,19 +19,3 @@ const answer = getTopCals(arr)
 
 console.log(answer)
 console.assert(answer === 71506, 'equals correct answer')
-
-const sum = (acc, val) => (acc += +val)
-const desc = (a, b) => b - a
-
-const topElf = (filename, n = 1) =>
-    fs
-        .readFileSync(filename)
-        .toString()
-        .split('\n\n')
-        .map((elf) => elf.split('\n').reduce(sum, 0))
-        .sort(desc)
-        .slice(0, n)
-        .reduce(sum, 0)
-
-console.log('1a:', topElf('./input.txt', 1))
-console.log('1b:', topElf('./input.txt', 3))
