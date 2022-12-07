f533e028549a1f2ba57d901a3b64089191690569
Ryan Gamel <ryan.gamel@gmail.com>
2022-12-07 10:48:10 -0600

    day 6 in Go

---
+++ Day-06/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	bytes, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	input := string(bytes)
+	one, two := findUniqueMarker(input, 4), findUniqueMarker(input, 14)
+	fmt.Printf("one: %v\ntwo: %v\n", one, two)
+}
+
+func findUniqueMarker(input string, size int) (index int) {
+	for i := size; i <= len(input); i++ {
+		if isNonRepeating(input[i-size : i]) {
+			return i
+		}
+	}
+	return -1
+}
+
+func isNonRepeating(str string) bool {
+	m := make(map[rune]int)
+	for _, b := range str {
+		if _, ok := m[b]; ok {
+			return false
+		}
+		m[b] = 0
+	}
+	return true
+}
